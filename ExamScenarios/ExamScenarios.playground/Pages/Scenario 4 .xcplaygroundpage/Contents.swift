8b9d03ef14a701b8d4e5fb50828eeaec5da98278
Student <student@mac-book-angus-mcleish.local>
2017-05-23 20:21:36 -0400

    hahahaha

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 4 .xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 4 .xcplaygroundpage/Contents.swift
@@ -30,8 +30,43 @@ let pink = Color(hue: 326, saturation: 99, brightness: 93, alpha: 100)
 let blue = Color(hue: 240, saturation: 80, brightness: 50, alpha: 50)
 let black = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
 
-// Your code below...
+// Background
+canvas.fillColor = offWhite
+canvas.drawRectangle(centreX: 200, centreY: 300, width: 400, height: 600)
 
+func roundRec (x: Int, y: Int) {
+    canvas.drawShapesWithBorders = false
+    if y + 20 == x || x == y + 420 || x == 300 && y == 80 {
+        canvas.fillColor = Color.init(hue: 37, saturation: 93, brightness: 98, alpha: 100)
+        canvas.drawRoundedRectangle(centreX: x, centreY: y, width: 95, height: 95)
+    } else {
+        canvas.fillColor = Color.init(hue: 326, saturation: 99, brightness: 93, alpha: 100)
+        canvas.drawRoundedRectangle(centreX: x, centreY: y, width: 95, height: 95)
+    }
+}
+
+for row in 0...4 {
+    let x = 0 + 100 * row
+    
+    for colom in 0...4 {
+        let y = -20 + 100 * colom
+        
+        roundRec(x: x, y: y)
+        
+        
+    }
+}
+
+for rows in 0...4 {
+    let x = -20 + 100 * rows
+    
+    for coloms in 0...4 {
+        let y = -50 + 100 * coloms
+        
+        canvas.fillColor = Color.init(hue: 240, saturation: 80, brightness: 50, alpha: 50)
+        canvas.drawRoundedRectangle(centreX: x, centreY: y, width: 95, height: 95)
+    }
+}
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
